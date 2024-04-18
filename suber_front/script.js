28500416771bfd86f103093150b2a1546f6398cd
thiagotassinari1 <126289953+thiagotassinari1@users.noreply.github.com>
2024-04-18 10:14:20 -0300

    estrutura suber

--- suber_front/script.js
+++ suber_front/script.js
@@ -4,22 +4,32 @@ button.onclick = async function() {
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
     let telefone = document.getElementById('telefone').value;
-    let data = {nome,email,telefone}
 
-    const response = await fetch('http://localhost:3001/api/store/usuario', {
-        method: 'POST',
-        headers: {'Content-type': 'application/json;charset=UTF-8'},
-        body: JSON.stringify(data)
-    });
-
-    let content = await response.json();
-
-    if(content.success) {
-        alert('Sucesso');
+    if (!nome || !telefone || !email) {
+        alert('Preencha todos os campos!');
+        return false
+    } else if (!email.includes('@')) {
+        alert('Email inválido!')
+        return false
     } else {
-        alert('Não');
+        let data = {nome,email,telefone}
+
+        const response = await fetch('http://localhost:3001/api/store/usuario', {
+            method: 'POST',
+            headers: {'Content-type': 'application/json;charset=UTF-8'},
+            body: JSON.stringify(data)
+        });
+    
+        let content = await response.json();
+    
+        if(content.success) {
+            alert('Sucesso!');
+        } else {
+            alert('Email ou telefone inválidos!');
+        }
+    
+        let reload = await content;
+        reload = location.reload()
     }
 
-    let reload = await content;
-    reload = location.reload()
 }
